83fa1c5afb2a182b8701777af8fce3c59af986bc
mickey english <mickeyeng92@gmail.com>
2018-07-31 03:51:42 +0100

    created controller, index and create methods

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,8 +1,20 @@
 module Api::V1
     class ContactsController < ApplicationController
         def index
-            @contacts = Contact.all
+            @contacts = Contact.order("created_at DESC")
             render json: @contacts
+        end 
+
+
+        def create
+            @contact = Contact.create(contact_params)
+            render json: @contact
+        end 
+
+        private
+
+        def contact_params
+            params.require(:contact).permit(:name, :email, :phone)
         end 
     end 
 end 
