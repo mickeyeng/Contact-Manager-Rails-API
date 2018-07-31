fe7a55d97442fcc660eecd3e42ba1e87a0df7049
mickey english <mickeyeng92@gmail.com>
2018-07-31 04:14:24 +0100

    created delete endpoint

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -5,11 +5,31 @@ module Api::V1
             render json: @contacts
         end 
 
+        def show
+            
+        end
+
 
         def create
             @contact = Contact.create(contact_params)
             render json: @contact
         end 
+
+        def update
+            @contact = Contact.find(params[:id])
+            @contact.update_attributes(contact_params)
+            render json: @contact
+        end 
+
+        def destroy
+            @contact = Contact.find(params[:id])
+
+            if @contact.destroy
+                head :no_content, status: :ok
+            else
+                render json: @contact.errors, status: :unprocessable_entity
+            end 
+        end
 
         private
 
